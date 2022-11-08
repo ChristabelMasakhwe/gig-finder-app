469656e5f3e4e6d9225eec9f21678923f44a11cc
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-11-08 17:30:38 +0300

    create-react-app

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <h1>GIG-FINDER</h1>
+      <p>Whether you are looking for a new job
+or just trying to make some extra money,
+our platform will connect you with 
+opportunities that suit your style and location.</p>
+     <button>REGISTER</button> 
+     </div>
+     <div>
+        <img src="Gig_finder_Frontend/gig-finder-app/src/assets/alex-starnes-PK_t0Lrh7MM-unsplash-removebg-preview.png" alt="hero-section" />
+     </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import {Link} from "react-router-dom";
+// // import Nav from 'react-bootstrap/Nav';
+// // import Navbar from 'react-bootstrap/Navbar';
+
+// function NavBar() {
+//     return (
+//       <div>
+       
+//         <NavBar className="navbar">
+//            <Link to="/home">Home</Link>
+  
+//         <Link to="/about">About us</Link>
+  
+//         <Link to="/category">category</Link>
+        
+//         <Link to="/listing">Job Listing</Link>
+//         </NavBar>
+//       </div>
+//     )
+//   }
+   
+//   export default NavBar;
