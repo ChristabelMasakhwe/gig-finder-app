bbf61154e2952159f7525bcff9cefe247848b652
Amos Langat <amos.langat@student.moringaschool.com>
2022-11-09 18:03:47 +0300

    add category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 import JobCard from './JobCard';
 
+
 function Category () {
+
+    const [jobs, setJobs] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:9292/job_listings")
+    .then((r) => r.json())
+    .then((jobs) => {
+        setJobs(jobs);
+    });
+}, []);
+
     return (
         <div id='category'>
+            <h1 className="main-title">Category</h1>
             <h1 id='header'>Job Type</h1>
             <label id='label'>select categories</label>
             <select name="job-type" id="job-type" form="job-category-form">
@@ -12,7 +25,16 @@ function Category () {
                 <option value="entertainment">Entertainment</option>
                 <option value="freelance">Freelance</option>
             </select>
-            <JobCard/>
+
+            <div className="job-list card">
+                {jobs.map((job, id) => {
+                return (<JobCard key={id} 
+                    title = {job.title}
+                    body = {job.body}
+                    date = {job.date}
+                ></JobCard>);
+                })}
+            </div>
         </div>
     )
 }
