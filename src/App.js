774fb1f3f5a56ded18907bfba6bb08139d3d4ff5
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-11-09 20:55:47 +0300

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
-import NavBar from "./components/NavBar"
-import Home from "./components/Home"
+import React, {useState, useEffect} from "react";
+// import NavBar from "./components/NavBar"
+// import Home from "./components/Home"
+// import About from "./components/About"
+import Newlisting from "./components/Newlisting"
 import "./App.css"
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [jobListings, setJobListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/categories")
+    .then((r) => r.json())
+    .then((data) => {
+      setCategories(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/job_listings")
+    .then((r) => r.json())
+    .then((jobListings) => {
+      setJobListings(jobListings);   
+    });
+  }, []);
+
+  // const addNewCategory = (category) => {
+  //   setCategories([...categories, category])
+  // }
+
   return (
     <div>
-    <NavBar />
+    {/* <NavBar />
     <Home />
+    <About /> */}
+    <Newlisting 
+    categories={categories} 
+    jobListings={jobListings} 
+    setJobListings={setJobListings}/>
     </div>
   );
 }
