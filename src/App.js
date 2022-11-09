11a554b7e87339119afd81168aeef9c88a3a36ee
Amos Langat <amos.langat@student.moringaschool.com>
2022-11-09 18:00:34 +0300

    add App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import NavBar from "./components/NavBar"
 import Home from "./components/Home"
+import Category from "./components/Category";
 import "./App.css"
+import "./category.css"
 
 function App() {
   return (
     <div>
     <NavBar />
-    <Home />
+    <Category />
     </div>
   );
 }
