4f03f0bc95dedc582c8ca13e8015f065b44e0897
Steve <steve.maina@student.moringaschool.com>
2022-11-09 13:39:01 +0300

    Add about content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar"
 import Home from "./components/Home"
+import About from "./components/About"
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
     <NavBar />
     <Home />
+    <About />
     </div>
   );
 }
