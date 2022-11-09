d769bcbfd2e267baab76c363f23349f2fbb593e5
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-11-09 20:26:29 +0300

    make form responsive

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,75 @@
 import React from 'react'
-import Logo from'../assets/Gig-finder (1).png';
+import { useState} from "react";
 
-const Register = () => {
+function Register() {
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+  
+    let handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        let res = await fetch("http://localhost:9292/users", {
+          method: "POST",
+          body: JSON.stringify({
+            name: name,
+            location: location,
+            email: email,
+            
+          }),
+        });
+        
+        if (res.status === 200) {
+          setName("");
+          setLocation("");
+          setEmail("");
+          setMessage("User created successfully");
+        } else {
+          setMessage("Some error occured");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
   return (
     <section>
-
-        <div className='register'>
-            <div className="col-1">
+        <div className="Register" style={{textAlign:"center", color:"#5A738E"}}>
                 <h2>New User</h2>
                 <span>Welcome to Gig-Finder!</span>
 
-                <form id="form" className="flex flex-col">
-                    <input type="text" placeholder="Username"/>
-                    <input type="text" placeholder="Email"/>
-                    <input type="text" placeholder="Location"/>
+                <div className="Column">
+                <form onSubmit={handleSubmit}> 
+                    <input 
+                       type="text" 
+                       value={name}
+                       placeholder="Username"
+                       onChange={(e) => setName(e.target.value)}
+                       />
+
+                     <input 
+                       type="text" 
+                       value={location}
+                       placeholder="Location"
+                       onChange={(e) => setLocation(e.target.value)}
+                       /> 
+
+                    <input 
+                      type="text"
+                      value={email}
+                      placeholder="Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      />
 
                     <button className="btn">Sign Up</button>
+
+                    <div className="message">{message ? <p>{message}</p> : null}</div>
                 </form>
             </div>
-            {/* <div className="col-2">
-                <img src={Logo} alt=""/>
-            </div> */}
         </div>
+        {/* </div> */}
     </section>
     
   )
