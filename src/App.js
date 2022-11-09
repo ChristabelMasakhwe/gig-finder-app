4eaf9de58ea55c76b407e7115b6b4f687f4e4eed
Steve <steve.maina@student.moringaschool.com>
2022-11-09 18:51:39 +0300

    Complete about us and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import NavBar from "./components/NavBar"
-import Home from "./components/Home"
+import About from "./components/About";
 import "./App.css"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-    <NavBar />
-    <Home />
+ 
+   <About />
+    <Footer />
     </div>
   );
 }
