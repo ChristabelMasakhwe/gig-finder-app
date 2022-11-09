3ea39b3fddadd94748697d36d6fce0bb482980f4
Ndiema-S <you@example.com>
2022-11-09 15:34:28 +0300

    edit hone and navbar components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,22 @@ import Alex from "../assets/Alex.png"
 function Home() {
   return (
     <div className='home'>
-
       <div className="container">
-        <div>
+        <div className="tagline">
           <div id="heading">
             <h1>GIG-FINDER</h1>
           </div>
-          <p id="paragraph">Whether you are looking for a new job
+          <p>Whether you are looking for a new job
             or just trying to make some extra money,
             our platform will connect you with
             opportunities that suit your style and location.</p>
-          <button>REGISTER</button>
+          <button id="button">REGISTER</button>
         </div>
+
         <div id="image">
           <img src={Alex} alt="hero-section" />
         </div>
+
       </div>
 
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,19 @@ import logo from "../assets/logo.png";
 
 function NavBar() {
     return (
-        <div className="navbar" >
-            <div class="logo">
-                <img src={logo} alt="logo" height="60px" />
+        <header>
+            <div className="navbar" >
+                <div class="logo">
+                    <img src={logo} alt="logo" height="60px" />
+                </div>
+                <div className="navlinks">
+                    <a href="/home">Home</a>
+                    <a href="/about">About us</a>
+                    <a href="/category">category</a>
+                    <a href="/listing">Job Listing</a>
+                </div>
             </div>
-            <div className="navlinks">
-                <a href="/home">Home</a>
-
-                <a href="/about">About us</a>
-
-                <a href="/category">category</a>
-
-                <a href="/listing">Job Listing</a>
-            </div>
-        </div>
+        </header>
     )
 }
 
